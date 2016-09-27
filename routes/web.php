fa91a1fb04c3eb7722a5cc36aae3c9ea6edbfaf1
limphilip <lim-philip@hotmail.com>
2016-09-26 20:39:25 -0400

    Created UserController
    UserController created, and route added.

--- routes/web.php
+++ routes/web.php
@@ -18,3 +18,9 @@ Route::get('/', function () {
 Auth::routes();
 
 Route::get('/home', 'HomeController@index');
+
+// ------------------------------------------------------------------------
+// Resource routes
+// ------------------------------------------------------------------------
+
+Route::resource('user', 'UserController');
