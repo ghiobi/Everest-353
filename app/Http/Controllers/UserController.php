cbaccd5d0206be7949f472cf51b5ffe723523d63
Laurendy Lam <lamlaurendy@gmail.com>
2016-09-30 20:41:30 -0400

    implement user edit

--- app/Http/Controllers/UserController.php
+++ app/Http/Controllers/UserController.php
@@ -6,6 +6,9 @@ use App\User;
 use Illuminate\Http\Request;
 
 use App\Http\Requests;
+use Illuminate\Support\Facades\Auth;
+use Illuminate\Support\Facades\File;
+use Intervention\Image\Facades\Image;
 
 class UserController extends Controller
 {
@@ -52,7 +55,7 @@ class UserController extends Controller
         $user = User::findOrFail($id);
 
         //Return view response
-        return view('users.show', compact('user'));
+        return view('user.show', compact('user'));
     }
 
     /**
@@ -63,7 +66,13 @@ class UserController extends Controller
      */
     public function edit($id)
     {
-        //
+        $user = User::findOrFail($id);
+
+        if (! $this->canModifyUser($user)){
+            return abort(403);
+        }
+
+        return view('user.edit', compact('user'));
     }
 
     /**
@@ -75,8 +84,82 @@ class UserController extends Controller
      */
     public function update(Request $request, $id)
     {
-        //
+        $user = User::findOrFail($id);
 
+        if (! $this->canModifyUser($user)){
+            return abort(403);
+        }
+
+        $this->validate($request, [
+            '_request' => 'required',
+            'first_name' => 'sometimes|required|max:255',
+            'last_name' => 'sometimes|required|max:255',
+            'email' => 'sometimes|required|email|max:255|unique:users',
+            'password' => 'sometimes|required|min:6|confirmed',
+            'is_suspended' => 'sometimes|required|boolean',
+            'address' => 'max:255',
+            'is_visible_address' => 'sometimes|required|boolean',
+            'birth_date' => 'date',
+            'is_visible_birth_date' => 'sometimes|required|boolean',
+            'license_num' => 'max:255',
+            'is_visible_license_num' => 'sometimes|required|boolean',
+            'is_visible_policies' => 'sometimes|required|boolean',
+            'external_email' => 'max:255|email',
+            'is_visible_external_email' => 'sometimes|required|boolean',
+            'avatar' => 'dimensions:min_width=300,min_height=300|image|max:5000' //is image type and max file size
+        ]);
+
+        switch ($request->_request){
+            case 'suspend' :
+                if (
+                    (Auth::user()->id == $user->id) ||
+                    (! Auth::user()->hasRole('admin')) ||
+                    (! Auth::user()->hasRole('super-admin') && Auth::user()->hasRole('admin') && $user->hasRole('admin'))
+                ){
+                    return abort(403);
+                }
+                $user->update($request->only('is_suspended'));
+                return back()->with('success', 'Account has been suspended.');;
+
+            case 'password':
+                if(
+                    Auth::user()->id != $user->id
+                ){
+                    return abort(403);
+                }
+                $user->update(['password' => bcrypt($request->password)]);
+                return back()->with('success', 'Password has been changed.');
+
+            case 'profile':
+
+                $user->policies = explode(';', $request->policies);
+
+                if (request()->hasFile('avatar')) {
+                    //Generating unique file name.
+                    $image_name = spl_object_hash(request()->file('avatar')) . '_' . time() . '.jpg';
+
+                    //Storing image
+                    Image::make(request()
+                        ->file('avatar'))
+                        ->encode('jpg')
+                        ->save(config('image.storage_path').'\\'.$image_name);
+
+                    File::delete(config('image.storage_path').'\\'.$user->avatar);
+                    $user->avatar = $image_name;
+                }
+
+                $user->update($request->except([
+                    'password',
+                    'is_suspended',
+                    'avatar',
+                    'policies'
+                ]));
+
+                $user->save();
+                return back()->with('success', 'Profile has been updated.');
+        }
+
+        return abort(403);
     }
 
     /**
@@ -89,4 +172,16 @@ class UserController extends Controller
     {
         //
     }
+
+    /**
+     * Determines if the current user can modify this user.
+     *
+     * @param User $user
+     * @return bool
+     */
+    private function canModifyUser(User $user)
+    {
+        return Auth::user()->id == $user->id || Auth::user()->hasRole('admin');
+    }
+
 }
