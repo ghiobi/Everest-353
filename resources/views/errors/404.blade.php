32f8c085fad7b07cf1c86cd9405014da3ef1b168
limphilip <lim-philip@hotmail.com>
2016-10-02 21:05:07 -0400

    Added page 404
    Added page 404

--- resources/views/errors/404.blade.php
+++ resources/views/errors/404.blade.php
@@ -1,5 +1,15 @@
 @extends('layouts.app')
 
 @section('content')
-
+    <div class="mdl-card mdl-shadow--3dp fwidth">
+        <div class="mdl-card__title padding-bottom--0">
+            <h2 class="mdl-card__title-text">Page Not Found</h2>
+        </div>
+        <div class="mdl-card__supporting-text padding-top--0">
+            <p>
+                The requested page has not been found. Please ensure the provided link is valid or go back
+                to the <a href="/">home page</a>.
+            </p>
+        </div>
+    </div>
 @endsection
