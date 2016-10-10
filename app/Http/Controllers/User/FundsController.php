529cccadcdddf8d7a45de99374cc2e6741fba1d2
limphilip <lim-philip@hotmail.com>
2016-10-10 17:05:54 -0400

    changes to funds controller
    - commented out the transaction part
    - added the withdrawal (substraction of funds in user's account only)
    - added the minimum balance a user must hold to perform withdrawal to
    setting seeder

--- app/Http/Controllers/User/FundsController.php
+++ app/Http/Controllers/User/FundsController.php
@@ -42,7 +42,7 @@ class FundsController extends Controller
         // Save
         $user->save();
 
-        // Confirm that the message has been sent successfully
+        // Confirm that the amount has been added successfully
         $display_amount = number_format($amount, 2, '.', ',');
         $display_balance = number_format($user->balance, 2, '.', ',');
         return back()->with('success', 'An amount of $'.$display_amount.' has been added to your account. Your balance is now $'.$display_balance.'.');
@@ -58,43 +58,75 @@ class FundsController extends Controller
     {
         // Validate request
         $this->validate($request, [
-            'amount' => 'required|numeric',
-            'recipient_id' => 'required|integer'
+            'amount' => 'required|numeric'
         ]);
 
         // Get all parameters from the request
         $user = Auth::user();
         $amount = $request->amount;
-        $recipient = User::find($request->recipient_id);
+        $minimumBalance = Setting::find('user_balance_withdraw')->value;
 
-        // Additional checks
-        if($recipient == null) {
-            return back()->withInput()->withErrors(['recipient' => 'The recipient of the transaction does not exist.']);
-        } elseif($amount <= 0) {
+        // Additional checks for amount
+        if($amount <= 0) {
             return back()->withInput()->withErrors(['amount' => 'The amount must be a positive number']);
         } elseif ((100 * $amount) - floor(100 * $amount) != 0) {
             return back()->withInput()->withErrors(['amount' => 'The number can only have two decimal digits']);
         } elseif ($user->balance < $amount) {
-            return back()->withInput()->withErrors(['amount' => 'You have insufficient funds to perform that transaction.']);
+            return back()->withInput()->withErrors(['amount' => 'You have insufficient funds to withdraw such amount.']);
+        } elseif ($user->balance < $minimumBalance) {
+            return back()->withInput()->withErrors(['amount' => 'Your account balance must at least hold $'. $minimumBalance . " in order to perform a withdrawal."]);
         }
 
-        // Get the admin user in order to add funds to its account
-        $admin = User::find(1);
-        $companyIncomePercentage = Setting::find('company_income_percentage')->value;
-
-        // Withdraw money from the user, give 95% to receiver, 5% to the system administrator
+        // Withdraw funds
         $user->balance -= $amount;
-        $recipient->balance += (1 - $companyIncomePercentage) * $amount;
-        $admin->balance += $companyIncomePercentage * $amount;
 
         // Save
         $user->save();
-        $recipient->save();
-        $admin->save();
 
-        // Confirm that the message has been sent successfully
+        // Confirm that the funds have been withdrawn
         $display_amount = number_format($amount, 2, '.', ',');
         $display_balance = number_format($user->balance, 2, '.', ',');
         return back()->with('success', 'An amount of $'.$display_amount.' has been withdrawn from your account. Your balance is now $'.$display_balance.'.');
+
+//        // Validate request
+//        $this->validate($request, [
+//            'amount' => 'required|numeric',
+//            'recipient_id' => 'required|integer'
+//        ]);
+//
+//        // Get all parameters from the request
+//        $user = Auth::user();
+//        $amount = $request->amount;
+//        $recipient = User::find($request->recipient_id);
+//
+//        // Additional checks
+//        if($recipient == null) {
+//            return back()->withInput()->withErrors(['recipient' => 'The recipient of the transaction does not exist.']);
+//        } elseif($amount <= 0) {
+//            return back()->withInput()->withErrors(['amount' => 'The amount must be a positive number']);
+//        } elseif ((100 * $amount) - floor(100 * $amount) != 0) {
+//            return back()->withInput()->withErrors(['amount' => 'The number can only have two decimal digits']);
+//        } elseif ($user->balance < $amount) {
+//            return back()->withInput()->withErrors(['amount' => 'You have insufficient funds to perform that transaction.']);
+//        }
+//
+//        // Get the admin user in order to add funds to its account
+//        $admin = User::find(1);
+//        $companyIncomePercentage = Setting::find('company_income_percentage')->value;
+//
+//        // Withdraw money from the user, give 95% to receiver, 5% to the system administrator
+//        $user->balance -= $amount;
+//        $recipient->balance += (1 - $companyIncomePercentage) * $amount;
+//        $admin->balance += $companyIncomePercentage * $amount;
+//
+//        // Save
+//        $user->save();
+//        $recipient->save();
+//        $admin->save();
+//
+//        // Confirm that the message has been sent successfully
+//        $display_amount = number_format($amount, 2, '.', ',');
+//        $display_balance = number_format($user->balance, 2, '.', ',');
+//        return back()->with('success', 'An amount of $'.$display_amount.' has been withdrawn from your account. Your balance is now $'.$display_balance.'.');
     }
 }
